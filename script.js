1e0dbf17c7b55d7bb181429583b0e5bcebb0cca8
Jomerjom <ekenesjoachim@gmail.com>
2023-12-04 09:49:21 +0100

    initial repo creation and array of animals

---
+++ script.js
@@ -0,0 +1,55 @@
+const animals = [
+    {
+      name: "Cat",
+      imgPath: "./img/Cat.jpg",
+      description: "Cats are small, carnivorous mammals that are often kept as pets. They are known for their agility, playfulness, and independent nature."
+    },
+    {
+      name: "Deer",
+      imgPath: "./img/Deer.jpg",
+      description: "Deers are herbivores known for their slender bodies and distinctive antlers, which are usually carried by the males. They are commonly found in forests and grasslands."
+    },
+    {
+      name: "Dog",
+      imgPath: "./img/Dog.jpg",
+      description: "Dogs are domesticated mammals, not natural wild animals. They are known for their loyalty and companionship with humans, making them popular pets."
+    },
+    {
+      name: "Elephant",
+      imgPath: "./img/Elephant.jpg",
+      description: "Elephants are large mammals known for their long trunks, tusks, and big ears. They are social animals and are often found in groups called herds."
+    },
+    {
+      name: "Koala",
+      imgPath: "./img/Koala.jpg",
+      description: "Koalas are marsupials native to Australia. They are known for their tree-dwelling lifestyle, primarily feeding on eucalyptus leaves."
+    },
+    {
+      name: "Lemur",
+      imgPath: "./img/Lemur.jpg",
+      description: "Lemurs are primates endemic to the island of Madagascar. They are known for their large, reflective eyes and distinctively long tails."
+    },
+    {
+      name: "Owl",
+      imgPath: "./img/Owl.jpg",
+      description: "Owls are nocturnal birds of prey known for their distinctive calls and the ability to rotate their heads up to 270 degrees. They are skilled hunters."
+    },
+    {
+      name: "Pelican",
+      imgPath: "./img/Pelican.jpg",
+      description: "Pelicans are large water birds with long bills and a distinctive throat pouch. They are known for their impressive fishing skills and soaring flight."
+    },
+    {
+      name: "Polar_Bear",
+      imgPath: "./img/Polar_bear.jpg",
+      description: "Polar bears are large carnivorous mammals native to the Arctic region. They are well-adapted to life in the icy environment and are excellent swimmers."
+    },
+    {
+      name: "Puffin",
+      imgPath: "./img/Puffin.jpg",
+      description: "Puffins are seabirds known for their distinctive appearance, with colorful beaks and markings. They are skilled flyers and divers, primarily found in the northern hemisphere."
+    }
+  ];
+  
+
+  
